ae91c6ac60f522d740c7ca24c75d3ea32f395b10
Matthew Fallon <matthewfallon33@gmail.com>
2018-03-09 20:45:04 +0000

    optimized function to operate better on iteration
    If the function was called multiple times newString would continue to be appended too, this is not what I want, so I set newString back to "" on every call of the function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var alpha = "abcdefghijklmnopqrstuvxyz";
-var newString = "";
+var newString;
 
 function generateRandomString(str){
+  newString = "";
   for(i = 0; i <= str.length-1; i++){
     randNum = Math.round(Math.random()*(str.length - 1));
     randLet = str[randNum];
